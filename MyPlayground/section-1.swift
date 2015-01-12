fdfa84d48e29fce389caf7859bf3d541246421b2
Chuck Konkol <ckonkol@gmail.com>
2015-01-11 19:31:15 -0600

    Swift Introduction
    Week 1

---
+++ MyPlayground/section-1.swift
@@ -0,0 +1,82 @@
+// Playground - noun: a place where people can play
+
+//Creat
+
+import UIKit
+
+/**Variables:
+String, Double, Float, Bool, Int
+
+When declaring variables use var [prefix: variable type; str, dbl,
+fl,bl,int][name of variable] = [value with quotes of string, without quotes if numeric]
+**/
+
+//example
+
+var strPlaceOfBirth = "Middletown, New York"
+
+var strYearsOld:Int = 47
+
+//showing variables format: \(variablename)
+
+//example
+
+var strMyAge = "I was born in \(strPlaceOfBirth) and my age is \(strYearsOld)"
+
+//make newline use \n
+
+//example:
+
+//new line
+var nl = "\n"
+
+//example
+
+var strPutTogether = "I was born in \(strPlaceOfBirth)"
+         "and my age is \(strYearsOld)"
+         
+         
+//println prints line
+         
+println("I was born in \(strPlaceOfBirth) and my age is \(strYearsOld)")
+
+
+//Exercise
+
+//declare string variable for your firstname
+
+
+
+//declare string variable for your lastname
+
+
+
+//declare string varibale for phone number
+
+
+
+//declare string variable for your email
+
+
+
+//declare integer varibale for your age
+
+
+/**Combine using println
+Example:
+
+My name is Chuck Konkol, I am 47 years old and my email addresss is ckonkol@gmail.com
+
+**/
+
+
+//Export .playground file and upload to Week 1 Assignment
+//File > Export > Week1.Playground
+
+    
+
+
+
+
+
+
