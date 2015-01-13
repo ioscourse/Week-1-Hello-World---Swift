860fa458686b55880a9f50c7cfb65af1d5a800c3
Chuck Konkol <ckonkol@gmail.com>
2015-01-13 09:58:51 -0600

    update

--- MyPlayground.playground/section-1.swift
+++ MyPlayground.playground/section-1.swift
@@ -1,25 +1,45 @@
 // Playground - noun: a place where people can play
-
-//Creat
+//Student Name
+//Date
+//CIS280
 
 import UIKit
 
 /**Variables:
 String, Double, Float, Bool, Int
 
+Variables CANNOT start with a number
+
 When declaring variables use var [prefix: variable type; str, dbl,
 fl,bl,int][name of variable] = [value with quotes of string, without quotes if numeric]
 **/
 
-//example
+//example of implicit type
 
-var strPlaceOfBirth = "Middletown, New York"
+var strPlaceOfBirth = "Middletown, NY"
 
-var strYearsOld:Int = 47
+//example of explicit type
+
+var strYearsOld:Int = 45
+
+//example of constant variable
+
+let intAge = 47
+
+//error if try to reassign constant
+
+//intAge = 48
+
+//example of variable that can be changed
+
+var intMyAge = 47
+
+intMyAge = 46
+
 
 //showing variables format: \(variablename)
 
-//example
+//example of putting variables togther
 
 var strMyAge = "I was born in \(strPlaceOfBirth) and my age is \(strYearsOld)"
 
@@ -30,15 +50,14 @@ var strMyAge = "I was born in \(strPlaceOfBirth) and my age is \(strYearsOld)"
 //new line
 var nl = "\n"
 
-//example
+//example with newline
 
-var strPutTogether = "I was born in \(strPlaceOfBirth)"
-         "and my age is \(strYearsOld)"
-         
-         
-//println prints line
+println( "I was born in \(strPlaceOfBirth) \(nl) and my age is \(strYearsOld) \(nl)")
+
+//println prints line without newline
          
 println("I was born in \(strPlaceOfBirth) and my age is \(strYearsOld)")
+
 
 
 //Exercise
@@ -70,7 +89,7 @@ My name is Chuck Konkol, I am 47 years old and my email addresss is ckonkol@gmai
 **/
 
 
-//Export .playground file and upload to Week 1 Assignment
+//Export .playground file, compress, rename to [lastname]week1HW.zip and upload to Week 1 Assignment
 //File > Export > Week1.Playground
 
     
